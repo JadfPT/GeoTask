a4842d7fbcaa0f4901026bbf4497bb120fc5ce91
JadfPT <jadf.3407@gmail.com>
2025-10-16 01:38:41 +0100

    Refactor main.dart to simplify main function and remove unused imports

--- geotask/lib/main.dart
+++ geotask/lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
-import 'services/notification_service.dart';
+import 'app.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService.instance.init(); // canal + permissões (Android 13+)
   runApp(const GeoTasksApp());
-}
-
-class GeoTasksApp extends StatelessWidget {
-  const GeoTasksApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GeoTasks',
-      theme: ThemeData.dark(useMaterial3: true),
-      home: const HomePage(),
-    );
-  }
 }
