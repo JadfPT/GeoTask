fae5b7e490e0031c289fbd1e466042eff03780a1
JadfPT <jadf.3407@gmail.com>
2025-10-27 20:54:13 +0000

    feat: Refactor GeoTasksApp to improve router setup and theme management

--- geotask/lib/app.dart
+++ geotask/lib/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
 import 'data/task_store.dart';
 import 'data/categories_store.dart';
 import 'router/app_router.dart';
+import 'theme/app_theme.dart';
 
 class GeoTasksApp extends StatefulWidget {
   const GeoTasksApp({super.key});
@@ -13,30 +15,29 @@ class GeoTasksApp extends StatefulWidget {
 }
 
 class _GeoTasksAppState extends State<GeoTasksApp> {
-  // cria o GoRouter uma única vez
-  late final _router = AppRouter.createRouter();
+  late final GoRouter _router = AppRouter.createRouter();
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<TaskStore>(create: (_) => TaskStore()),
-        ChangeNotifierProvider<CategoriesStore>(create: (_) => CategoriesStore()),
+        ChangeNotifierProvider(create: (_) => TaskStore()),
+        ChangeNotifierProvider<CategoriesStore>(
+          create: (_) => CategoriesStore()..load(),
+        ),
+        ChangeNotifierProvider(create: (_) => ThemeController()),
       ],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        routerConfig: _router,
-        // usa Material 3; se já tens um AppTheme, podes trocar aqui
-        theme: ThemeData(
-          useMaterial3: true,
-          brightness: Brightness.light,
-          colorSchemeSeed: const Color(0xFF7C4DFF),
-        ),
-        darkTheme: ThemeData(
-          useMaterial3: true,
-          brightness: Brightness.dark,
-          colorSchemeSeed: const Color(0xFF7C4DFF),
-        ),
+      child: Builder(
+        builder: (context) {
+          final mode = context.watch<ThemeController>().mode;
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false, // 3) tira o “DEBUG” do canto
+            routerConfig: _router,
+            theme: AppTheme.light,
+            darkTheme: AppTheme.dark,
+            themeMode: mode,
+          );
+        },
       ),
     );
   }
